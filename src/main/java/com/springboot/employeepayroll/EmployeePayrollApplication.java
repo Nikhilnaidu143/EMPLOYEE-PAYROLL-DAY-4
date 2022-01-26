e4b38c1f3474246bab5b996d68f87fc6649757b9
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-26 18:20:34 +0530

    UC-3.3:- Created application.properties for dev and prod environments.

--- src/main/java/com/springboot/employeepayroll/EmployeePayrollApplication.java
+++ src/main/java/com/springboot/employeepayroll/EmployeePayrollApplication.java
@@ -2,6 +2,7 @@ package com.springboot.employeepayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,8 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
-		log.info("Employee Payroll App Started...!");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment...!" , context.getEnvironment().getProperty("environment"));
 	}
 
 }
