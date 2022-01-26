091b11dc4df00dca459085b429b7be36e152aecd
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-26 17:07:27 +0530

    UC-3.2:- Used lombok library for logging.

--- src/main/java/com/springboot/employeepayroll/EmployeePayrollApplication.java
+++ src/main/java/com/springboot/employeepayroll/EmployeePayrollApplication.java
@@ -3,11 +3,15 @@ package com.springboot.employeepayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll App Started...!");
 	}
 
 }
